4a339f4f3c025446b704a59bf82021c9d231e2c0
Ashish Mehra <99390725+ashishMehra79@users.noreply.github.com>
2022-07-27 11:33:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import Podcast from './Component/Podcast/Podcast';
 import Signup from './Component/Signup/Signup';
 import Footer from './Component/Footer/Footer'
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import PopupCards from './Component/PopupCards/PopupCards';
+import React, {useState} from 'react;
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <>
-    <Router>
+//     <Router>
+    <div className= 'popup-container'>
+    {(!buttonPopup) ?
+    <button onClick= {()=>setButtonPopup(true)}>Share
+      <i className= 'fa-solid fa-chevron-down chevron'></i>
+    </button>
+    :
+    <button onClick= {()=>setButtonPopup(false)}>Share
+      <i className= 'fa-solid fa-xmark xmark'></i>
+    </button>
+    }
+    </div>
+    <PopupCards trigger = {buttonPopup}/>
       <Podcast />
       <Signup />
       <Footer />
-    </Router>
+//     </Router>
 
     </>
   );
