9db80319fccf9108d86580bfb9a23f552b3003e4
Ashish Mehra <99390725+ashishMehra79@users.noreply.github.com>
2022-07-27 11:59:22 +0530

    Update PopupCards.js

--- src/Component/PopupCards/PopupCards.js
+++ src/Component/PopupCards/PopupCards.js
@@ -1,1 +1,24 @@
+import './popupCards.css';
+function PopupCards(props){
+  return(props.trigger) ? (
+    <div className='popup-card'>
+      <ul className='article-share'>
+        <li className='article-shareItem share-icon'>
+          <a><i classname='fa-brands fa-facebook-square'></i></a>
+        </li>
+        <li className='article-shareItem share-icon'>
+          <a><i classname='fa-brands fa-twitter'></i></a>
+        </li>
+        <li className='article-shareItem share-icon'>
+          <a><i classname='fa-solid fa-link'></i></a>
+        </li>
+        <li className='article-shareItem share-icon'>
+          <a><i classname='fa-solid fa-envelope'></i></a>
+        </li>
+      </ul>
+    </div>
+  ): "";
+  
+}
 
+export default PopupCards;
